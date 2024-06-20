b6472102fd99042a43139f1310593f71d25e53cd
ZutkijVoron <zutkijvoron44@gmail.com>
2024-06-20 16:32:31 +0500

    вроде готовое задание

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 document.querySelector('.link__button').addEventListener('click', () => {
 	document.querySelector('.link__button svg').classList.toggle('rotate')
 })
+
+function getScrollbarWidth() {
+	return window.innerWidth - document.documentElement.clientWidth
+}
+
+document.documentElement.style.setProperty(
+	'--scrollbar-width',
+	`${getScrollbarWidth()}px`
+)
+
+const elementsBefore = document.querySelector('*::before')
+
+for (const element in elementsBefore) {
+	element.style.width = `calc(100vw - var(--scrollbar-width))`
+}
